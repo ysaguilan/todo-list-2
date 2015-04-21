22c7a26045fb834c505eb95ad6f25c4c6ae3fbdb
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-21 09:06:06 -0700

    Milestone4: - Created connect.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php 
+$mysqli = new mysqli("localhost", "root", "root", "todo2");
+// if connect error connection dies
+if ($mysqli->connect_error) {
+	die('Connect error ( ' . $mysqli->connect_errno . ')')
+	. $mysqli->connect_error);
+}
+else {
+	echo "connection made";
+}
+$mysqli->close();
+?>
