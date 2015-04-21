4b9a5a0f1b41d7f0feaa0fc349453f133aa17b09
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-21 09:15:17 -0700

    Milstone 5 and 5.1: - Moved connect.php to new folder(includes) - Fixed syntax error

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,7 @@
 $mysqli = new mysqli("localhost", "root", "root", "todo2");
 // if connect error connection dies
 if ($mysqli->connect_error) {
-	die('Connect error ( ' . $mysqli->connect_errno . ')')
+	die('Connect error ( ' . $mysqli->connect_errno . ')'
 	. $mysqli->connect_error);
 }
 else {
