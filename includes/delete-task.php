6aaefc50bd5c17c53388ab3abab408831562d73f
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-23 08:52:26 -0700

    Milestone 13: - Created delete-task.php In includes Folder

---
+++ includes/delete-task.php
@@ -0,0 +1,9 @@
+<?php 
+$task_id = strip_tags($_POST["id"]);
+require('connect.php');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
+//deletes results in query where = task_id
+if ($result = $mysqli->query("DELETE FROM tasks WHERE id='task_id'")) {
+		# code...
+	}	
+ ?>
