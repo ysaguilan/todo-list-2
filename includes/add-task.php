e9f31e55f593458b9d0dbff08105d3ab31c5b24f
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-23 08:43:26 -0700

    Milestone 11: - Created if Statement To Take Results From Query

--- includes/add-task.php
+++ includes/add-task.php
@@ -10,4 +10,15 @@
 	$mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
 // Gets Info From Database
 	$query = "SELECT * FROM todo2 WHERE task='$task' and date='$date' and time='$time' ";
+//takes result of query
+	if ($result=$mysqli->query($query)) {
+		while ($row=$result->fetch_assoc()) {
+			$task_id= $row['id'];
+			$task_name=$row['task'];
+		}
+	}
+
+	$mysqli->close();
+
+	echo '<li><span>' . $task_name . '</span><img id="' . $task_id . '" class="delete-button" width="10px" src="images/close.svg"/></li>';
  ?>
