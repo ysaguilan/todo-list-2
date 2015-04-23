9e0459412386698a0c7b6f47ae1878cdf24e90b3
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-23 09:16:16 -0700

    Milestone 15: - Added Code To php Require On Line 11 In index.php

--- index.php
+++ index.php
@@ -8,7 +8,24 @@
 	<div class="wrap">
 	<div class="task-list">
 		<ul>
-		<?php	require("includes/connect.php"); ?>
+		<?php	require("includes/connect.php");
+		$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
+		//ASC = ascending
+		$query = "SELECT * FROM tasks ORDER BY  date ASC, time ASC";
+		if ($result = $mysqli->query($query)) {
+			$numrows = $result->num_rows;
+			if ($numrows>0) {
+				while ($row = $result->fetch_assoc()) {
+					$task_id = $row['id'];
+					$task_name = $row['task'];
+
+					echo "<li>
+					<span>'.$task_name'
+					";
+				}
+			}
+		}
+		?>
 		</ul>
 	</div>
 	<form class="add-new-task" autocomplete="off">
