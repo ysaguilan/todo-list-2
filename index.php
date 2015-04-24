4de8fade4fc4aa1cb33cce93c36b413ad532fa11
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-23 17:33:54 -0700

    Milestone16: - Added delete-button Image In index.php

--- index.php
+++ index.php
@@ -19,9 +19,10 @@
 					$task_id = $row['id'];
 					$task_name = $row['task'];
 
-					echo "<li>
-					<span>'.$task_name'
-					";
+				echo '<li>
+				<span>' . $task_name . '</span>
+				<img id="' . $task_id . '" class="delete-button" width= "10px" src="images/close.svg">
+				</li>';
 				}
 			}
 		}
